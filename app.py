113f6829211f4f62d3db188193cc1711a687fb01
kghite <kghite@gmail.com>
2017-08-23 19:22:43 -0400

    UPDATE: Procfile

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('html/index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
