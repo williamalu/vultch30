71baa32b695d135727e7a4d926523e831d58d081
kghite <kghite@gmail.com>
2017-08-23 22:42:33 -0400

    ADD: Working app with latest design

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
 from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
-"""
-from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
@@ -28,4 +10,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run(debug=True)
-"""
